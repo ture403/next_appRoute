316710e8e51c3bb394010f9b79c634ca188baac4
전윤기 <93423185+ture403@users.noreply.github.com>
2025-06-07 18:56:35 +0900

    update ul

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,4 +1,18 @@
-export default async function Page({ searchParams }: { searchParams: Promise<{ q: string }> }) {
-    const { q } = await searchParams;
-    return <div>search page : {q}</div>;
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const { q } = await searchParams;
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
