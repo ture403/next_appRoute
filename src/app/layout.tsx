83ce9f607b16a748c8ecd93a87348c87a66ea9af
전윤기 <ture403@warmhilt.com>
2025-06-11 09:14:23 +0900

    modify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,34 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+async function Footer() {
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_API_SEVER_URL}/book`);
+
+    // const datas: BookData[] = await res.json();
+    // const len = datas.length;
+    // console.log(len);
+
+    return <footer>제작 @YG</footer>;
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <div className={style.container}>
-          <header>
-            <Link href={"/"}>📚 ONEBITE BOOKS</Link>
-          </header>
-          <main>{children}</main>
-          <footer>제작 @winterlood</footer>
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <div className={style.container}>
+                    <header>
+                        <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+                    </header>
+                    <main>{children}</main>
+                    <Footer />
+                </div>
+            </body>
+        </html>
+    );
 }
