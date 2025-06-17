d849497ea78477419d3b1890dc4772c429f79102
전윤기 <93423185+ture403@users.noreply.github.com>
2025-06-17 21:22:10 +0900

    병렬 라우터

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,28 @@
-import "./globals.css";
-import Link from "next/link";
-import style from "./layout.module.css";
+import './globals.css';
+import Link from 'next/link';
+import style from './layout.module.css';
+import { ReactNode } from 'react';
 
 export default function RootLayout({
-  children,
+	children,
+	modal,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
+	modal: ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <div className={style.container}>
-          <header>
-            <Link href={"/"}>📚 ONEBITE BOOKS</Link>
-          </header>
-          <main>{children}</main>
-          <footer>제작 @winterlood</footer>
-        </div>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body>
+				<div className={style.container}>
+					<header>
+						<Link href={'/'}>📚 ONEBITE BOOKS</Link>
+					</header>
+					<main>{children}</main>
+					<footer>제작 @winterlood</footer>
+				</div>
+				{modal}
+				<div id='modal-root'></div>
+			</body>
+		</html>
+	);
 }
--- src/components/review-item-delete-button.tsx
+++ src/components/review-item-delete-button.tsx
@@ -1,23 +1,38 @@
-"use client";
+'use client';
 
-import { DeleteReviewAction } from "@/actions/delete-review.ation";
-import { useActionState, useEffect, useRef } from "react";
+import { DeleteReviewAction } from '@/actions/delete-review.ation';
+import { useActionState, useEffect, useRef } from 'react';
 
-export default function ReviewItemDeleteButton({ reviewId, bookId }: { reviewId: number; bookId: number }) {
-    const formRef = useRef<HTMLFormElement>(null);
-    const [state, formAction, isPending] = useActionState(DeleteReviewAction, null);
+export default function ReviewItemDeleteButton({
+	reviewId,
+	bookId,
+}: {
+	reviewId: number;
+	bookId: number;
+}) {
+	const formRef = useRef<HTMLFormElement>(null);
+	const [state, formAction, isPending] = useActionState(
+		DeleteReviewAction,
+		null
+	);
 
-    useEffect(() => {
-        if (state && !state.status) {
-            alert(state.error);
-        }
-    }, [state]);
+	useEffect(() => {
+		if (state && !state.status) {
+			alert(state.error);
+		}
+	}, [state]);
 
-    return (
-        <form ref={formRef} action={formAction}>
-            <input name="reviewId" value={reviewId} hidden />
-            <input name="bookId" value={bookId} hidden />
-            {isPending ? <div>...</div> : <div onClick={() => formRef.current?.requestSubmit()}>삭제하기</div>}
-        </form>
-    );
+	return (
+		<form ref={formRef} action={formAction}>
+			<input name='reviewId' value={reviewId} hidden readOnly />
+			<input name='bookId' value={bookId} hidden readOnly />
+			{isPending ? (
+				<div>...</div>
+			) : (
+				<div onClick={() => formRef.current?.requestSubmit()}>
+					삭제하기
+				</div>
+			)}
+		</form>
+	);
 }
